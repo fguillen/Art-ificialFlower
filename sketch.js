4a9168383a5902056dc0ae8c66968eaae364747e
Fernando Guillen <fguillen.mail@gmail.com>
2022-01-16 19:09:15 +0100

    circle

--- sketch.js
+++ sketch.js
@@ -1,23 +1,59 @@
 let points;
+let pointsIndex;
+let resetAt;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
   points = new Array(4);
 
   points[0] = createVector(5, 26);
   points[1] = createVector(73, 24);
   points[2] = createVector(73, 61);
   points[3] = createVector(15, 65);
+
+  pointsIndex = 0;
+  resetTime();
 }
 
 function draw() {
   background(220);
 
+  strokeWeight(5);
+  point(points[0].x, points[0].y);
+  point(points[1].x, points[1].y);
+  point(points[2].x, points[2].y);
+  point(points[3].x, points[3].y);
+  strokeWeight(1);
+
   noFill();
-  stroke(255, 102, 0);
-  curve(points[0].x, points[0].y, points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y);
-  stroke(0);
-  curve(points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y, points[3].x, points[3].y);
-  stroke(255, 102, 0);
-  curve(points[1].x, points[1].y, points[2].x, points[2].y, points[3].x, points[3].y, points[3].x, points[3].y);
+  beginShape();
+  curveVertex(points[3].x, points[3].y);
+  curveVertex(points[0].x, points[0].y);
+  curveVertex(points[1].x, points[1].y);
+  curveVertex(points[2].x, points[2].y);
+  curveVertex(points[3].x, points[3].y);
+  curveVertex(points[0].x, points[0].y);
+  curveVertex(points[1].x, points[1].y);
+
+  endShape();
+
+  if(Date.now() > resetAt) {
+    resetPoints();
+  }
+}
+
+function resetPoints() {
+  pointsIndex = 0;
+  resetTime();
+}
+
+function resetTime() {
+  resetAt = Date.now() + 2_000;
+  console.log("resetTime()");
+}
+
+function mouseClicked() {
+  points[pointsIndex % 4] = createVector(mouseX, mouseY);
+  pointsIndex ++;
+  resetTime();
 }
