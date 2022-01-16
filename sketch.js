88c90ed95867b39128d5d9ae734423972b6e6a07
Fernando Guillen <fguillen.mail@gmail.com>
2022-01-16 19:11:45 +0100

    5 points

--- sketch.js
+++ sketch.js
@@ -4,12 +4,13 @@ let resetAt;
 
 function setup() {
   createCanvas(800, 800);
-  points = new Array(4);
+  points = new Array(5);
 
   points[0] = createVector(5, 26);
   points[1] = createVector(73, 24);
   points[2] = createVector(73, 61);
   points[3] = createVector(15, 65);
+  points[4] = createVector(10, 65);
 
   pointsIndex = 0;
   resetTime();
@@ -23,15 +24,17 @@ function draw() {
   point(points[1].x, points[1].y);
   point(points[2].x, points[2].y);
   point(points[3].x, points[3].y);
+  point(points[4].x, points[4].y);
   strokeWeight(1);
 
   noFill();
   beginShape();
-  curveVertex(points[3].x, points[3].y);
+  curveVertex(points[4].x, points[4].y);
   curveVertex(points[0].x, points[0].y);
   curveVertex(points[1].x, points[1].y);
   curveVertex(points[2].x, points[2].y);
   curveVertex(points[3].x, points[3].y);
+  curveVertex(points[4].x, points[4].y);
   curveVertex(points[0].x, points[0].y);
   curveVertex(points[1].x, points[1].y);
 
@@ -53,7 +56,7 @@ function resetTime() {
 }
 
 function mouseClicked() {
-  points[pointsIndex % 4] = createVector(mouseX, mouseY);
+  points[pointsIndex % points.length] = createVector(mouseX, mouseY);
   pointsIndex ++;
   resetTime();
 }
